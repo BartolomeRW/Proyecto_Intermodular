c011a1882a45f9bf13e18634133534600c6cdf55
BartolomeRW <wawrzyniakedyta110@gmail.com>
2025-11-14 00:08:17 +0100

    Se modifica todo

--- MASIA/ProyectoCurso/Codigo/CargarDatos.py
+++ MASIA/ProyectoCurso/Codigo/CargarDatos.py
@@ -15,24 +15,32 @@ def leer_mysql(host, user, password, database, tabla):
         df = pd.read_sql(query, conn)
     return df
 
-def leer_mariadb(host, user, password, database, tabla):
-    url = f"mysql+pymysql://{user}:{password}@{host}/{database}"
-    engine = create_engine(url, echo=False)
+import mysql.connector
+import pandas as pd
 
-    with engine.connect() as conn:
-        return pd.read_sql(f"SELECT * FROM {tabla}", conn)
+def leer_mariadb(host, user, password, database, tabla):
+    conn = mysql.connector.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=database
+    )
+    query = f"SELECT * FROM {tabla}"
+    df = pd.read_sql(query, conn)
+    conn.close()
+    return df
 
 def leer_sqlite(ruta_sqlite, tabla):
-    url = f"sqlite:///{ruta_sqlite}"
-    engine = create_engine(url)
+    import sqlite3
+    conn = sqlite3.connect(ruta_sqlite)
     query = f"SELECT * FROM {tabla}"
-    with engine.connect() as conn:
-        df = pd.read_sql(query, conn)
+    df = pd.read_sql(query, conn)
+    conn.close()
     return df
 
-def leer_sqlalchemy(url, tabla):
+from sqlalchemy import create_engine
+
+def leer_sqlalchemy(url, query):
     engine = create_engine(url)
-    query = f"SELECT * FROM {tabla}"
     with engine.connect() as conn:
-        df = pd.read_sql(query, conn)
-    return df
+        return pd.read_sql(query, conn)
