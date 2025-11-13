c0d43890cc159cfe1e7570ba02d586b1e191c790
BartolomeRW <wawrzyniakedyta110@gmail.com>
2025-11-13 23:57:00 +0100

    Se modifica todo

--- MASIA/ProyectoCurso/Codigo/CargarDatos.py
+++ MASIA/ProyectoCurso/Codigo/CargarDatos.py
@@ -4,49 +4,33 @@ from sqlalchemy import create_engine
 def leer_csv(ruta):
     return pd.read_csv(ruta)
 
-# ----------------------------
-# MYSQL
-# ----------------------------
 def leer_mysql(host, user, password, database, tabla):
     url = f"mysql+mysqlconnector://{user}:{password}@{host}/{database}"
     engine = create_engine(url)
     query = f"SELECT * FROM {tabla}"
-
     with engine.connect() as conn:
         df = pd.read_sql(query, conn)
     return df
 
-# ----------------------------
-# MARIADB
-# ----------------------------
 def leer_mariadb(host, user, password, database, tabla):
     url = f"mariadb+mariadbconnector://{user}:{password}@{host}/{database}"
     engine = create_engine(url)
     query = f"SELECT * FROM {tabla}"
-
     with engine.connect() as conn:
         df = pd.read_sql(query, conn)
     return df
 
-# ----------------------------
-# SQLITE
-# ----------------------------
 def leer_sqlite(ruta_sqlite, tabla):
     url = f"sqlite:///{ruta_sqlite}"
     engine = create_engine(url)
     query = f"SELECT * FROM {tabla}"
-
     with engine.connect() as conn:
         df = pd.read_sql(query, conn)
     return df
 
-# ----------------------------
-# SQLALCHEMY (GENÉRICO)
-# ----------------------------
 def leer_sqlalchemy(url, tabla):
     engine = create_engine(url)
     query = f"SELECT * FROM {tabla}"
-
     with engine.connect() as conn:
         df = pd.read_sql(query, conn)
     return df
