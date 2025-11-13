f0f2cacfa6e4eff012241cfa38b4e98851da0fb7
BartolomeRW <wawrzyniakedyta110@gmail.com>
2025-11-13 23:26:34 +0100

    Se modifica todo

--- MASIA/ProyectoCurso/Codigo/CargarDatos.py
+++ MASIA/ProyectoCurso/Codigo/CargarDatos.py
@@ -4,27 +4,49 @@ from sqlalchemy import create_engine
 def leer_csv(ruta):
     return pd.read_csv(ruta)
 
+
+# ----------------------------
+# LECTOR MYSQL
+# ----------------------------
 def leer_mysql(host, user, password, database, tabla):
     url = f"mysql+mysqlconnector://{user}:{password}@{host}/{database}"
     engine = create_engine(url)
 
-    query = f"SELECT * FROM {tabla};"
+    query = f"SELECT * FROM {tabla}"
     df = pd.read_sql(query, engine)
     return df
 
-def leer_mariadb(host, user, password, database, tabla):
-    url = f"mysql+pymysql://{user}:{password}@{host}/{database}"
-    engine = create_engine(url, echo=False)
-    with engine.connect() as conn:
-        return pd.read_sql(f"SELECT * FROM {tabla}", conn)
 
+# ----------------------------
+# LECTOR MARIADB
+# ----------------------------
+def leer_mariadb(host, user, password, database, tabla):
+    url = f"mariadb+mariadbconnector://{user}:{password}@{host}/{database}"
+    engine = create_engine(url)
+
+    query = f"SELECT * FROM {tabla}"
+    df = pd.read_sql(query, engine)
+    return df
+
+
+# ----------------------------
+# LECTOR SQLITE
+# ----------------------------
 def leer_sqlite(ruta_sqlite, tabla):
     url = f"sqlite:///{ruta_sqlite}"
     engine = create_engine(url)
-    with engine.connect() as conn:
-        return pd.read_sql(f"SELECT * FROM {tabla}", conn)
 
-def leer_sqlalchemy(url, query):
+    query = f"SELECT * FROM {tabla}"
+    df = pd.read_sql(query, engine)
+    return df
+
+
+# ----------------------------
+# LECTOR SQLALCHEMY
+# ----------------------------
+def leer_sqlalchemy(url, tabla):
     engine = create_engine(url)
-    with engine.connect() as conn:
-        return pd.read_sql(query, conn)
+
+    query = f"SELECT * FROM {tabla}"
+    df = pd.read_sql(query, engine)
+    return df
