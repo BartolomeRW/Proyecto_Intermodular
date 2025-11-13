e28b87d14213f35f9dab30cc864040dd33a399b2
BartolomeRW <wawrzyniakedyta110@gmail.com>
2025-11-13 21:49:04 +0100

    Actualización del proyecto Flask + IA

--- Practicas/MASIA/ProyectoCurso/Codigo/CargarDatos.py
+++ Practicas/MASIA/ProyectoCurso/Codigo/CargarDatos.py
@@ -6,13 +6,15 @@ def leer_csv(ruta):
 
 def leer_mysql(host, user, password, database, tabla):
     url = f"mysql+pymysql://{user}:{password}@{host}/{database}"
-    engine = create_engine(url)
-    return pd.read_sql(f"SELECT * FROM {tabla}", engine)
+    engine = create_engine(url, echo=False)
+    with engine.connect() as conn:
+        return pd.read_sql(f"SELECT * FROM {tabla}", conn)
 
 def leer_mariadb(host, user, password, database, tabla):
-    url = f"mysql+mysqlconnector://{user}:{password}@{host}/{database}"
-    engine = create_engine(url)
-    return pd.read_sql(f"SELECT * FROM {tabla}", engine)
+    url = f"mysql+pymysql://{user}:{password}@{host}/{database}"
+    engine = create_engine(url, echo=False)
+    with engine.connect() as conn:
+        return pd.read_sql(f"SELECT * FROM {tabla}", conn)
 
 def leer_sqlite(ruta_sqlite, tabla):
     url = f"sqlite:///{ruta_sqlite}"
@@ -22,6 +24,3 @@ def leer_sqlite(ruta_sqlite, tabla):
 def leer_sqlalchemy(url, query):
     engine = create_engine(url)
     return pd.read_sql(query, engine)
-
-def guardar_prediccion(origen, datos):
-    ...
